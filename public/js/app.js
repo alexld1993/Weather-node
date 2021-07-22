38b64238892058c82ec9c5c55e42e8a068af21d3
Alex's Macbook <themac@Alexs-MacBook-Pro.local>
2021-07-22 19:42:51 -0400

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -10,16 +10,14 @@ weatherForm.addEventListener("submit", (e) => {
 
   const location = search.value;
 
-  fetch(`http://localhost:3000/weather/?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          firstMsg.textContent = data.error;
-        } else {
-          firstMsg.textContent = data.location;
-          secondMsg.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch(`/weather/?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        firstMsg.textContent = data.error;
+      } else {
+        firstMsg.textContent = data.location;
+        secondMsg.textContent = data.forecast;
+      }
+    });
+  });
 });
